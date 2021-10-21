7843a47a92be911b6b62febe24751619c066fa15
stuart.warren <stuart.warren@ocado.com>
2021-10-21 10:40:17 +0000

    fix: auto format

--- tests/test_longest_palendromic_substring.py
+++ tests/test_longest_palendromic_substring.py
@@ -12,17 +12,22 @@ def solution(s: str) -> str:
 def test_is_palendrome_a():
     assert solution("a") == "a", "test 1 should be a"
 
+
 def test_is_palendrome_aaaab():
     assert solution("aaaab") == "aaaa", "test 2 should be aaaa"
+
 
 def test_is_palendrome_baaaa():
     assert solution("baaaa") == "aaaa", "test 3 should be aaaa"
 
+
 def test_is_palendrome_babcbad():
     assert solution("babcbad") == "abcba", "test 4 should be abcba"
 
+
 def test_is_palendrome_aaabcccc():
     assert solution("aaabcccc") == "cccc", "test 5 should be cccc"
+
 
 def test_is_palendrome_abcdefgh():
     assert solution("abcdefgh") == "a", "test 6 should be a"
